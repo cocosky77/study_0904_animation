57053617407cc7d8dedc572bda757f957ca2eeb3
[cocosky77] <[skybella@naver.com]>
2024-09-04 15:48:36 +0900

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [animate, setAnimate] = useState(false);
+
+  const startAnimation = () => {
+    setAnimate(true);
+
+    // 애니메이션이 끝나면 상태를 false로 돌리기
+    setTimeout(() => {
+      setAnimate(false);
+    }, 1000); // 애니메이션의 지속 시간에 맞게 설정 (1초)
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+      <div className="container">
+      {/* 애니메이션 상태에 따라 drop-animation 클래스를 추가 */}
+      <div className={`box ${animate ? 'drop-animation' : ""}`}></div>
+      <button onClick={startAnimation} className="btn">애니메이션 시작</button>
+      </div>
 
+    </div>
+  )
+}
 export default App;
